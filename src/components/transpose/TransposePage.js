f1c1eaddc13c84c1bf58530c0774fa96df6a7a09
benjaminng <shen_soonng@hotmail.com>
2020-05-30 13:24:52 +0800

    using state and bind method for value changes

--- src/components/transpose/TransposePage.js
+++ src/components/transpose/TransposePage.js
@@ -1,58 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button } from 'react-bootstrap'
 
 import './transpose.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function TransposePage() {
-  return (
-    <div>
-      <h1>Transpose</h1>
-      <header className="App-header">
-        <p>
-          Welcome to Transpose (sorry lack of better words) Utility!
-        </p>
-      </header>
+export default class TransposePage extends Component {
 
-      <div className="container">
-        <div id="st-box">
-          <h3>Data Set 1</h3>
-          <textarea
-            className="form-control"
-            id="exampleFormControlTextarea1"
-            rows="15"
-          />
-        </div>
-        <div id="nd-box">
-          <h3>Data Set 2</h3>
-          <textarea
-            className="form-control"
-            id="exampleFormControlTextarea2"
-            rows="15"
-          />
-        </div>
+  constructor(props) {
+    super(props);
 
-        <div id="ld-box">
-          <div></div>
-          <Button variant="primary">Process</Button>
-        </div>
+    this.state = {
+      dataset1: '',
+      dataset2: '',
+      dataset3: '',
+    };
 
-        <div id="ld-box">
-          <h3>Result</h3>
-          <textarea
-            className="form-control"
-            id="exampleFormControlTextarea2"
-            rows="15"
-          />
+    // This binding is necessary to make `this` work in the callback
+    this.handleDataSet1Change = this.handleDataSet1Change.bind(this);
+    this.handleDataSet2Change = this.handleDataSet2Change.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleDataSet1Change(event) {
+    this.setState({dataset1: event.target.value});
+  }
+
+  handleDataSet2Change(event) {
+    this.setState({dataset2: event.target.value});
+  }
+
+  handleButtonClick() {
+    this.setState({dataset3: 'hello'});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Transpose</h1>
+        <header className="App-header">
+          <p>
+            Welcome to Transpose (sorry lack of better words) Utility!
+          </p>
+        </header>
+  
+        <div className="container">
+          <div id="st-box">
+            <h3>Data Set 1</h3>
+            <textarea
+              className="form-control"
+              value={this.state.dataset1}
+              onChange={this.handleDataSet1Change}
+              id="dataset1"
+              rows="15"
+            />
+          </div>
+          <div id="nd-box">
+            <h3>Data Set 2</h3>
+            <textarea
+              className="form-control"
+              value={this.state.dataset2}
+              onChange={this.handleDataSet2Change}
+              id="dataset2"
+              rows="15"
+            />
+          </div>
+  
+          <div id="ld-box">
+            <div></div>
+            <Button onClick={this.handleButtonClick} variant="primary">Process</Button>
+          </div>
+  
+          <div id="ld-box">
+            <h3>Result</h3>
+            <textarea
+              className="form-control"
+              disabled
+              value={this.state.dataset3}
+              id="dataset3"
+              rows="15"
+            />
+          </div>
+          
         </div>
-        
       </div>
-
-      
-      
-
-    </div>
-  );
+    );
+  }
 }
-
-export default TransposePage;
